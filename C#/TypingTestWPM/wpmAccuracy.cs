97e3c151724731beba897f13835de0d0b3bcb569
CyrehnJeward <160415015+CyrehnJeward@users.noreply.github.com>
2024-03-14 22:29:50 +0800

    Add files via upload

---
+++ C#/TypingTestWPM/wpmAccuracy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermProject
+{
+    internal class wpmAccuracy
+    {
+        public static double grossWPMs = 0;
+        public static double amountofBacks = 0;
+        public static double accuracy = 0;
+        public static double j = 0;
+        public static void wordPMAccuracy(double x, double y, double z)
+        {
+            double allEntires = (Convert.ToDouble(x) - Convert.ToDouble(y)) / 5;
+            double totalminute = Convert.ToDouble(z) / 60;
+            double gWPM = allEntires / totalminute;
+
+            grossWPMs = Math.Round(gWPM);
+
+            amountofBacks = x - y;
+            accuracy = (100 - ((x - amountofBacks) / x * 100));
+            j = Math.Truncate(accuracy * 100) / 100;
+        }
+    }
+}
