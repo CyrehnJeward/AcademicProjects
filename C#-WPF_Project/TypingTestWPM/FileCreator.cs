6938aa2fc39caf092164f180cfc386b1de6b581a
CyrehnJeward <160415015+CyrehnJeward@users.noreply.github.com>
2024-03-14 22:25:40 +0800

    Add files via upload

---
+++ C#-WPF_Project/TypingTestWPM/FileCreator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermProject
+{
+    internal class FileCreator
+    {
+        public static void fileWriter(bool appendFlag, string message)
+        {
+            SaveFileDialog dialog = new SaveFileDialog()
+            {
+                Filter = "Text|*.txt|All|*.*"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, message);
+            }
+        }
+    }
+}
